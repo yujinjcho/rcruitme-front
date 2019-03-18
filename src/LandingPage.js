86167942cdb86fed1fbce3777e8aa7921a9c5cc4
Yujin <yujinjcho@gmail.com>
2019-03-17 19:29:48 -0700

    add sections

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,10 +4,18 @@ import './mainSection.css';
 
 const LandingPage = () => {
   return (
-    <div className="main-section-container">
-      <div className="main-image">
-        <h1 className="main-header">Catchy Title</h1>
-        <p className="main-description" >Catchy description that explains value proposition</p>
+    <div>
+      <div className="main-section-container">
+        <div className="main-image">
+          <h1 className="main-header">Catchy Title</h1>
+          <p className="main-description" >Catchy description that explains value proposition</p>
+        </div>
+      </div>
+      <div>
+        todo: description with bullets
+      </div>
+      <div>
+        todo: generic footer
       </div>
     </div>
   );
