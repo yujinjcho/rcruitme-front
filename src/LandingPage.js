1547abfdebe42ba8e3cb164a7f997680ad5dbac6
Yujin <yujinjcho@gmail.com>
2019-03-19 21:10:05 -0700

    add description bullets and footer

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,18 +5,32 @@ import './mainSection.css';
 const LandingPage = () => {
   return (
     <div>
+
       <div className="main-section-container">
         <div className="main-image">
-          <h1 className="main-header">Catchy Title</h1>
-          <p className="main-description" >Catchy description that explains value proposition</p>
+          <h1 className="main-header">Rcruitme</h1>
+          <p className="main-description" >Manage and track your career opportunities</p>
         </div>
       </div>
-      <div>
-        todo: description with bullets
+
+      <Container>
+        <div className="description-section-container">
+          <h3 className="description-title">Track Opportunities</h3>
+          <p>Keep track of job positions in a centralized location</p>
+
+          <h3 className="description-title">Market Intelligence</h3>
+          <p>Learn about different companies in your area and salary ranges</p>
+
+          <h3 className="description-title">Relationships</h3>
+          <p>Build a network of recruiters who can help you when want to consider new opportunities</p>
+        </div>
+      </ Container>
+
+      <div className="footer">
+        Rcruitme
       </div>
-      <div>
-        todo: generic footer
-      </div>
+
+
     </div>
   );
 }
